c33fbd5981bea4d26cb5eb3b4bdd7f3f65b578f7
Harley Day <harley.day4@gmail.com>
2018-10-22 14:01:44 +0100

    use the smarthost for imperial email dispatch

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # matlab-Imperial-email-setup
 
-Note that this code will connect you to the Imperial College London email servers only. To connect to another institution, you may have to edit some of the server addresses, port numbers, username formats etc... If you have an Imperial emil address, then the code should work as it is. Just follow the instructions in the GUI!
-
-Thanks to Jesse Lai for [this contribution](https://uk.mathworks.com/matlabcentral/fileexchange/19729-passwordentrydialog) to MATLAB file exchange.
+Note that this code will connect you to the Imperial College London email servers only.
--- emailSetup.m
+++ emailSetup.m
@@ -1,17 +1,6 @@
 %% setup sending from Imperial College email account
-[password, mailfrom] = passwordEntryDialog('enterUserName', true, 'DefaultUserName', '@ic.ac.uk', 'PasswordLengthMin',0, 'PasswordLengthMax', inf);
-
-setpref('Internet','E_mail',mailfrom);
-setpref('Internet','SMTP_Server','smtp.office365.com');
-setpref('Internet','SMTP_Username',mailfrom);
-setpref('Internet','SMTP_Password',password);
-props = java.lang.System.getProperties;
-props.setProperty('mail.smtp.auth','true');
-props.setProperty('mail.smtp.socketFactory.class','javax.net.ssl.SSLSocketFactory');
-props.remove('mail.smtp.socketFactory.class');  %Some recommendations included commenting out the line above and using this line instead to remove the SSL ...?
-props.setProperty('mail.smtp.port','587');  %have tried this line, and the below line, and both, with ports 465, 587, and 25.  Port 465 works with google.
-props.setProperty('mail.smtp.socketFactory.port','587'); 
-props.setProperty('mail.smtp.starttls.enable', 'true' );  %was recommended for the ports 587 and 25
+setpref('Internet','SMTP_Server','smarthost.cc.ic.ac.uk');
+setpref('Internet','E_mail','root@ic.ac.uk');
 
 %% construct message
 to = 'someone@domain';
@@ -19,8 +8,8 @@ subject = 'From MATLAB';
 message = {'First line.'; 'Second line.'};
 attachment = {'file_for_attachment.svg'};
 
-% edit the to string, and uncomment the lines below to send an email.
-%sendmail(to,...
+%% edit the to string, and uncomment the lines below to send an email.
+% sendmail(to,...
 %     subject,...
 %     message,...
 %     attachment);
